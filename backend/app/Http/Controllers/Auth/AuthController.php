9c0313c9f896dc91ce171829e2a62b4e50a7b7ec
Oleksandr Sokolianskiy <alexsokolianskiy@gmail.com>
2024-06-08 11:30:55 +0300

    create basic auth

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -2,13 +2,49 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(LoginRequest $request)
     {
-        
+        $loginUserData = $request->validated();
+        $user = User::where('email',$loginUserData['email'])->first();
+        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $registerUserData = $request->validated();
+        $user = User::create([
+            'name' => $registerUserData['name'],
+            'email' => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
+
+    public function logout() 
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+          "message"=>"logged out"
+        ]);
     }
 }
