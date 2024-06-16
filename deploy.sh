4e97f1e363f46dd9147c945076aa1f45d499c27c
Oleksandr Sokolianskiy <alexsokolianskiy@gmail.com>
2024-06-16 15:58:59 +0300

    wip

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ cd backend
 # Pull the latest version of the app
 git pull origin main
 
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
 # Clear the old cache
 php artisan clear-compiled
 
