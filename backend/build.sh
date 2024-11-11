4d00be88b6ce223d9b14cf23bbba9f8c6029768f
Oleksandr Sokolianskiy <alexsokolianskiy@gmail.com>
2024-11-11 22:27:01 +0200

    wip

--- backend/build.sh
+++ backend/build.sh
@@ -9,4 +9,5 @@ rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 
 # Run arduino-cli compile command
+/usr/loca/bin/arduino-cli core install arduino:avr --config-file /var/www/html/.arduino15/arduino-cli.yaml
 /usr/local/bin/arduino-cli compile -b arduino:avr:uno -e --output-dir "$BUILD_DIR" "$SKETCH_DIR" --config-file /var/www/html/.arduino15/arduino-cli.yaml 
